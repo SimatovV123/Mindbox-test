8a5773ceb6e35dacaff48944065ad6a465df6097
Vadim Simatov <Vadim.Simatov2@icl-services.com>
2023-11-09 16:18:46 +0300

    Добавил тесты и второе задание

---
+++ ProductsQuery.sql
@@ -0,0 +1,7 @@
+﻿select p.Name as Product, c.Name as Category
+from Products p
+left join ProductCats pc
+on pc.ProductId = p.Id
+left join Categories c
+on c.Id = pc.CategoryId
+group by p.Name, c.Name
---
+++ TestData.sql
@@ -0,0 +1,121 @@
+﻿CREATE TABLE Products
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+    Price INT,
+    Name NVARCHAR(20),
+	Count Int
+)
+
+CREATE TABLE Categories
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+    Name NVARCHAR(20),
+)
+
+CREATE TABLE ProductCats
+(
+    Id UNIQUEIDENTIFIER PRIMARY KEY,
+    ProductId UNIQUEIDENTIFIER,
+	CategoryId UNIQUEIDENTIFIER,
+	FOREIGN KEY (ProductId)  REFERENCES Products (Id),
+	FOREIGN KEY (CategoryId)  REFERENCES Categories (Id)
+)
+
+insert into Products values (NEWID(), 50, 'Rye bread', 12)
+insert into Products values (NEWID(), 55, 'Wheat bread', 10)
+insert into Products values (NEWID(), 30, 'Bun', 12)
+insert into Products values (NEWID(), 150, 'Herring', 12)
+insert into Products values (NEWID(), 130, 'Mackerel', 12)
+insert into Products values (NEWID(), 150, 'Chicken', 12)
+insert into Products values (NEWID(), 400, 'Beef', 12)
+insert into Products values (NEWID(), 200, 'Chicken fillet', 12)
+insert into Products values (NEWID(), 70, 'Macaron', 12)
+insert into Products values (NEWID(), 70, 'Gum', 12)
+insert into Products values (NEWID(), 70, 'Apple juice', 12)
+
+
+insert into Categories values (NEWID(), 'Bread')
+insert into Categories values (NEWID(), 'Meat')
+insert into Categories values (NEWID(), 'Poultry meat')
+insert into Categories values (NEWID(), 'Fish')
+insert into Categories values (NEWID(), 'Bakery')
+insert into Categories values (NEWID(), 'Flour')
+
+insert into ProductCats values 
+(newId(),
+(select Id from Products where Name = 'Rye bread'),
+(select Id from Categories where Name = 'Bread'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Rye bread'),
+(select Id from Categories where Name = 'Bakery'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Rye bread'),
+(select Id from Categories where Name = 'Flour'))
+
+insert into ProductCats values 
+(newId(),
+(select Id from Products where Name = 'Wheat bread'),
+(select Id from Categories where Name = 'Bread'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Wheat bread'),
+(select Id from Categories where Name = 'Bakery'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Wheat bread'),
+(select Id from Categories where Name = 'Flour'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Macaron'),
+(select Id from Categories where Name = 'Flour'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Bun'),
+(select Id from Categories where Name = 'Flour'))
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Bun'),
+(select Id from Categories where Name = 'Bakery'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Herring'),
+(select Id from Categories where Name = 'Fish'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Mackerel'),
+(select Id from Categories where Name = 'Fish'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Chicken'),
+(select Id from Categories where Name = 'Meat'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Chicken'),
+(select Id from Categories where Name = 'Poultry meat'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Beef'),
+(select Id from Categories where Name = 'Meat'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Chicken fillet'),
+(select Id from Categories where Name = 'Meat'))
+
+insert into ProductCats values
+(newId(), 
+(select Id from Products where Name = 'Chicken fillet'),
+(select Id from Categories where Name = 'Poultry meat'))
